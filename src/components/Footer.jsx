bae4c542050958c2ba407d46523a06c8af0fd272
Shakshi Vishwakarma <vishwakarmasakshi80@gmail.com>
2024-08-24 14:48:42 +0530

    changes in footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div className="text-black w-screen z-50 max-w-screen-2xl mx-auto md:px-20 h-20 flex items-center justify-center">
+        <div className="text-white bg-black w-screen z-50 max-w-screen-2xl mx-auto md:px-20 h-20 flex items-center justify-center">
             <div className="flex flex-wrap justify-center">
                 <p className="font-bold">
                     Created By <span className="text-green-600">Shakshi Vishwakarma</span> | &#169; 2024 All rights reserved.
